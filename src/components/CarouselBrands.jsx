a4662bfb58e04cad53f988825f9d747335cb9bdc
tobiajras <tobiajras@hotmail.com>
2023-08-31 11:18:31 -0300

    brands carousel functionality

--- src/components/CarouselBrands.jsx
+++ src/components/CarouselBrands.jsx
@@ -2,6 +2,9 @@ import useEmblaCarousel from 'embla-carousel-react';
 import Autoplay from 'embla-carousel-autoplay';
 import { useCallback } from 'react';
 
+import { IoMdArrowRoundForward } from 'react-icons/io';
+import { IoMdArrowRoundBack } from 'react-icons/io';
+
 function CarouselBrands() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
 
@@ -15,99 +18,111 @@ function CarouselBrands() {
 
   return (
     <div className='flex justify-center items-center'>
-      <div ref={emblaRef} className='overflow-hidden max-w-6xl mx-3 sm:mx-10'>
-        <div className='flex gap-10'>
-          <div className='flex items-center justify-center gap-10 flex-[0_0_100%]'>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/champion.svg'
-                alt='champion logo'
-              />
+      <div className='flex justify-center items-center gap-3 md:gap-10 max-w-6xl mx-3 sm:mx-20 w-full'>
+        <button
+          className='flex justify-center items-center bg-white h-full rounded-full p-1 md:p-3 shadow-md'
+          onClick={scrollPrev}
+        >
+          <IoMdArrowRoundBack className='md:w-8 md:h-8' />
+        </button>
+        <div ref={emblaRef} className='overflow-hidden'>
+          <div className='flex gap-10'>
+            <div className='flex items-center justify-center gap-6 sm:gap-10 md:gap-16 flex-[0_0_100%]'>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/champion.svg'
+                  alt='champion logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/nike.svg'
+                  alt='nike logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/fila.svg'
+                  alt='fila logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/converse.svg'
+                  alt='converse logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/gucci.svg'
+                  alt='gucci logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/offwhite.svg'
+                  alt='offwhite logo'
+                />
+              </div>
             </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/nike.svg'
-                alt='nike logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/fila.svg'
-                alt='fila logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/converse.svg'
-                alt='converse logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/gucci.svg'
-                alt='gucci logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/offwhite.svg'
-                alt='offwhite logo'
-              />
-            </div>
-          </div>
-          <div className='flex items-center justify-center gap-10 flex-[0_0_100%]'>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/newbalance.svg'
-                alt='newbalance logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/adidas.svg'
-                alt='adidas logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/vans.svg'
-                alt='vans logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/puma.svg'
-                alt='puma logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/underarmour.svg'
-                alt='underarmour logo'
-              />
-            </div>
-            <div className='w-24 h-24'>
-              <img
-                className='w-full h-full'
-                src='/assets/brands/dc.svg'
-                alt='dc logo'
-              />
+            <div className='flex items-center justify-center gap-6 sm:gap-10 md:gap-16 flex-[0_0_100%]'>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/newbalance.svg'
+                  alt='newbalance logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/adidas.svg'
+                  alt='adidas logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/vans.svg'
+                  alt='vans logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/puma.svg'
+                  alt='puma logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/underarmour.svg'
+                  alt='underarmour logo'
+                />
+              </div>
+              <div className=''>
+                <img
+                  className='w-full h-full'
+                  src='/assets/brands/dc.svg'
+                  alt='dc logo'
+                />
+              </div>
             </div>
           </div>
         </div>
-        <button onClick={scrollPrev}>Prev</button>
-        <button onClick={scrollNext}>Next</button>
+        <button
+          className='flex justify-center items-center bg-white h-full rounded-full p-1 md:p-3 shadow-md'
+          onClick={scrollNext}
+        >
+          <IoMdArrowRoundForward className='md:w-8 md:h-8' />
+        </button>
       </div>
     </div>
   );
